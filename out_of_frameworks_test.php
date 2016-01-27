358840a52c7e0c8037aa8ee7e9acad7720c57df1
apryakhin <8s7sKodA0646>
2016-01-27 15:31:33 +0300

    Easy testing support

---
+++ out_of_frameworks_test.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AlexKex\route_finder;
+
+require __DIR__."/test_autoload.php";
+
+$map2 = [
+    ["A", "B", 3],
+    ["A", "C", 4],
+    ["B", "E", 6],
+    ["B", "D", 5],
+    ["C", "D", 2],
+    ["C", "F", 10],
+    ["D", "F", 1],
+    ["D", "E", 1],
+    ["E", "F", 7]
+];
+
+$finder = new routeFinder();
+
+$start = "A";
+$finish = "F";
+$info = $finder->findRoute($start, $finish, $map2);
+
+echo "Route between ".$start." and ".$finish." \nRoute price : ".$info["price"]." \nTransit points : ".implode(" ", $info["route"])."\n";
---
+++ test_autoload.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apryakhin
+ * Date: 27.01.16
+ * Time: 14:35
+ */
+
+function autoload($className)
+{
+    $className = ltrim($className, '\\');
+    $fileName  = __DIR__."/";
+    $namespace = '';
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName .= "/src/".str_replace('_', DIRECTORY_SEPARATOR, $className) . '.class.php';
+
+    require $fileName;
+}
+spl_autoload_register('autoload');
